ed9a2e33a1356a6d0ce2ad68305262d30f03e4da
YostinMejia <110141509+YostinMejia@users.noreply.github.com>
2022-08-22 15:34:34 -0500

    Update reto4.py
    solo falta graficar

--- reto4.py
+++ reto4.py
@@ -1,17 +1,32 @@
+from datetime import date
+
+#Creo los productos, creo el carrito , cuando vaya a consolidar le mando el carrito a la orden y devuelvo la orden
+
 class User():
-    def __init__(self,id,user_name,balance,order_list):
+    def __init__(self,id,user_name,balance):
         self.id=int(id)
         self.user_name=str(user_name)
         self.balance=float(balance)
-        self.order_list=list(order_list)
+        
+        self.order_list=[]
+        self.car=[]
     
-    #Se envia a orden
-    def add_product_to_car(self,id_product):
-        self.order_list.append(str(id_product))
+    #Se agrega el carrito
+    def add_product_to_car(self,product):
+        self.car.append(product)
 
-    #no se     
+    #añadimos la compra al historial
     def consolidate_order(self,order_id):
-        return 
+
+        for i in self.car:
+            order_id.total+=i.price
+        if order_id.total>self.balance:
+            print(f" NO TIENE SALDO SUFICIENTE \n El valor total de su compra es de {order_id.total} y su saldo es de {self.balance}$ ")
+        else:
+            print(f" La orden se ha realizado con exito.\nEl valor a pagar es de {order_id.total}")
+            order_id.product_list.append(self.car)
+            self.order_list.append(order_id)
+    
 
     #añadir saldo al usuario
     def add_to_balance(self,amount):
@@ -20,27 +35,18 @@ class User():
         else:
             print("digite un número valido")
 
-    #no se
+    #Graficacion 
     def plot_order_history(self):
         pass
 
-juan=User(2,"juan marinilla",1231.123,[])
-juan.add_product_to_car(123)
-juan.add_to_balance(12)
-print(juan.balance)
-
 class Order():
-    def __init__(self,usuario,id,date,total,status):
-        self.usuario=usuario
+    def __init__(self,id,date,status=False,total=0):
         self.id=int(id)
-        self.product_list=usuario.order_list     #tomamos la lista del usuario
+        self.product_list=[]
         self.date=date
-        self.total=float(total)
         self.status=status
-
-ca112d=Order(juan,1,"12/23/2222",123123,True)
-print(ca112d.product_list)
-
+        self.total=total
+        
 class Product():
     def __init__(self, id, name, price, dict):
         self.id=int(id)
@@ -52,10 +58,26 @@ class Product():
         self.price=float(price)
         self.price_history[date]=price
 
-papa=Product(2,"papa",12.3,{})
-print(papa.price)
-print(papa.price_history)
+papa=Product(1,"papa",123,{date(2022,1,12):123})
+yuca=Product(2,"yuca",12,{date(2022,1,19):12})
+platano=Product(3,"platano",34,{date(2022,1,2):34})
+zapote=Product(4,"zapote",42,{date(2022,1,5):42})
+coco=Product(5,"coco",54,{date(2022,1,4):54})
 
-papa.update_price("12/321/3",123)
-print(papa.price)
-print(papa.price_history)
+papa.update_price(date(2022,3,12),2)
+
+juan=User(1,"juan maria",13.12)
+juan.add_product_to_car(yuca)
+juan.add_product_to_car(platano)
+juan.add_product_to_car(zapote)
+for i in range (len(juan.car)):
+    print(juan.car[i].name)
+
+mercado=Order(1,date(2022,1,2))
+juan.consolidate_order(mercado)
+
+juan.add_to_balance(3123123)
+mercado=Order(1,date(2022,1,2))
+juan.consolidate_order(mercado)
+
+juan.plot_order_history()
